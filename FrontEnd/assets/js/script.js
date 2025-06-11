55229790e272eb58ff7b62affc7ae05984d5f31e
Drelall <137816034+Drelall@users.noreply.github.com>
2025-06-11 10:55:52 +0200

    maj3 catégories

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,4 +1,8 @@
-async function getWorks() { //!partie 1, importation des données
+//?Etape 1.2 les projets qui s'affichent, gallerie en provenance du backend
+
+//!partie 1, importation des données
+
+async function getWorks() { 
     const url = "http://localhost:5678/api/works";// URL de l'API pour récupérer les œuvres
     try {
         const response = await fetch(url);
@@ -27,3 +31,35 @@ figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
 
 document.querySelector(".gallery").append(figure); // Ajout de la figure au corps du document
 }
+
+//?la possibilité de filtrer la galerie par catégorie de projet, qui aura été généré dynamiquement.
+
+async function getCategories() { 
+    const url = "http://localhost:5678/api/categories";// URL de l'API pour récupérer les catégories
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        for (let i = 0; i < json.length; i++) {// Boucle pour parcourir les catégories récupérées
+          setFilter(json[i]); // Appel de la fonction pour créer le filtre avec les données récupérées 
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getCategories();// Appel de la fonction pour récupérer les catégories
+
+
+
+function setFilter(data) { 
+    const div = document.createElement("div");
+div.innerHTML = `${data.name}`;
+document.querySelector(".div-container").append(div); 
+}
+
+
+//?fin : Etape 1.2 les projets qui s'affichent, gallerie en provenance du backend
