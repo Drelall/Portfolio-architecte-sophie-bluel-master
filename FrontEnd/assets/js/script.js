eebd471829e774f2a1fb813ccc247e36f3e86880
Drelall <137816034+Drelall@users.noreply.github.com>
2025-06-12 12:04:44 +0200

    maj6 catégories

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -3,30 +3,30 @@
 //!partie 1, importation des données
 
 async function getWorks(filter) {
+  document.querySelector(".gallery").innerHTML= ""; // Réinitialisation de la galerie avant de charger les œuvres
   const url = "http://localhost:5678/api/works"; // URL de l'API pour récupérer les œuvres
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-     
-      const json = await response.json();
-      const filtered = json.filter((data) => data.categoryId === 1); // Filtrage des œuvres par catégorie (id = 1)
-      if(filter === undefined) {
-        for (let i = 0; i < json.length; i++) {
-          setFigure(json[i]);
-        }
-  }else {
-  for (let i = 0; i < filtered.length; i++) {
-  console.log(filtered[i]);
-  setFigure(filtered[i]); // Appel de la fonction pour créer la figure avec les données filtrées
+    const json = await response.json();
+    if (filter) {
+      const filtered = json.filter((data) => data.categoryId === filter); // Filtrage des œuvres par catégorie si un filtre est spécifié
+      for (let i = 0; i < filtered.length; i++) {
+        setFigure(filtered[i]); // Appel de la fonction pour créer une figure avec les données récupérées
+    }
+  } else{
+    for (let i = 0; i < json.length; i++) {
+        setFigure(json[i]); // Appel de la fonction pour créer une figure avec les données récupérées
     }
   }
+    
   } catch (error) {
     console.error(error.message);
   }
 }
-getWorks(); // Appel de la fonction pour récupérer les œuvres
+getWorks(); // Appel de la fonction pour récupérer les œuvres et les afficher dans la galerie
 
 
 //!partie 2, création de la figure
@@ -66,15 +66,12 @@ getCategories(); // Appel de la fonction pour récupérer les catégories
 function setFilter(data) {// Fonction pour créer un filtre avec les données d'une catégorie
   console.log(data);
   const div = document.createElement("div");
-  div.addEventListener("click", () => alert("salut"));// Ajout d'un écouteur d'événement pour afficher une alerte lors du clic
   div.className = data.id; // Ajout de la classe correspondant à l'id de la catégorie
-  document
-  .querySelector(data[i])
-  .addEventListener("click", console.log(data.id)); 
+  div.addEventListener("click", () => getWorks(data.id)); 
   div.innerHTML = `${data.name}`;
   document.querySelector(".div-container").append(div);
 }
-//document.querySelector(".tous").addEventListener('click', () =>getWorks()); // Ajout d'un écouteur d'événement pour afficher une alerte lors du clic sur le bouton "Tous"
+document.querySelector(".tous").addEventListener('click', () =>getWorks()); // Ajout d'un écouteur d'événement pour afficher une alerte lors du clic sur le bouton "Tous"
 
 
 //?fin : Etape 1.2 les projets qui s'affichent, gallerie en provenance du backend
